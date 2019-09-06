5bb3cc5378b8d592bb80eb702f8d429577995445
Michael Tai <michael@downloadthebear.com>
2019-09-06 11:07:36 -0400

    - better track of the verb and noun using CLICMD

--- Sources/demogod/CLIContext.swift
+++ Sources/demogod/CLIContext.swift
@@ -2,26 +2,64 @@
 //  CLIContext.swift
 //  demogod
 //
-//
-
 import Foundation
 
-//struct HW: Codable {
-//    var hello: String = "world"
-//    var foo: String?
-//}
-//
-//let jData = try! JSONSerialization.data(withJSONObject: ["hello": "worrrrld"], options: .prettyPrinted)
-//
-////let jData = try! JSONSerialization.data(withJSONObject: [:], options: .prettyPrinted)
-//
-//print((try? JSONDecoder().decode(HW.self, from: jData)) ?? HW())
-
-
-enum CLINounTypes: String, CaseIterable {
-    case server = "server"
+enum CliCmd {
+    enum ServerVerb {
+        case run(CLIRequestServerContext?)
+        case stop
+    }
+    case server(ServerVerb)
 }
+
+extension CliCmd {
+    var noun: String {
+        switch self {
+        case .server(_): return "server"
+        }
+    }
+    
+    var verb: String {
+        switch self {
+        case .server(let verb):
+            switch verb {
+            case .run(_): return "run"
+            case .stop: return "stop"
+            }
+        }
+    }
+    
+    var cmd: String {
+        switch self {
+        case .server(let vb):
+            switch vb {
+            case .run(let ctx): return CliCmd.buildCmd(noun, verb, ctx?.JSONString)
+            case .stop: return CliCmd.buildCmd(noun, verb, nil)
+            }
+        }
+    }
+    
+    static func buildCmd(_ noun: String,_ verb: String,_ ctx: String?) -> String {
+        return [noun, verb, ctx, "\n"].compactMap({$0}).joined(separator: " ")
+    }
+}
+
+
 
 struct CLIRequestServerContext: Codable {
     var port: Int = 8090
 }
+
+
+
+extension Encodable {
+    var JSONString: String {
+        guard let jData = try? JSONSerialization.data(withJSONObject: self),
+            let result = String(data: jData, encoding: .utf8) else {
+                return ""
+        }
+        return result
+    }
+}
+
+
--- Sources/demogod/main.swift
+++ Sources/demogod/main.swift
@@ -14,32 +14,41 @@ cli.register { (output) in
     Log.debug(output)
 }
 
-cli.register(command: CLICommand(noun: CLINounTypes.server.rawValue,
-                                 verb: "run",
-                                 action: { [weak server] (context) -> (Error?) in
-                                    guard let server = server,
-                                        let jData = context.data(using: .utf8) else {
-                                            return NSError.MAINRequestServerMissing
-                                    }
-                                    let context = (try? JSONDecoder().decode(CLIRequestServerContext.self, from: jData))
-                                        ?? CLIRequestServerContext()
-                                    server.run(onPort: context.port)
-                                    return nil
-    }, help: { () -> (String) in
-        return ""
-}))
+let _ = {
+    let cmd = CliCmd.server(.run(nil))
+    cli.register(command: CLICommand(noun: cmd.noun,
+                                     verb: cmd.verb,
+                                     action: { [weak server] (context) -> (Error?) in
+                                        guard let server = server,
+                                            let jData = context.data(using: .utf8) else {
+                                                return NSError.MAINRequestServerMissing
+                                        }
+                                        let context = (try? JSONDecoder().decode(CLIRequestServerContext.self, from: jData))
+                                            ?? CLIRequestServerContext()
+                                        server.run(onPort: context.port)
+                                        return nil
+        }, help: { () -> (String) in
+            return ""
+    }))
+}()
 
-cli.register(command: CLICommand(noun: CLINounTypes.server.rawValue,
-                                 verb: "stop",
-                                 action: { [weak server] (context) -> (Error?) in
-                                    guard let server = server else {
-                                        return NSError.MAINRequestServerMissing
-                                    }
-                                    server.stop()
-                                    return nil
-    }, help: { () -> (String) in
-        return ""
-}))
+
+let _ = {
+    let cmd = CliCmd.server(.stop)
+    cli.register(command: CLICommand(noun: cmd.noun,
+                                     verb: cmd.verb,
+                                     action: { [weak server] (context) -> (Error?) in
+                                        guard let server = server else {
+                                            return NSError.MAINRequestServerMissing
+                                        }
+                                        server.stop()
+                                        return nil
+        }, help: { () -> (String) in
+            return ""
+    }))
+}()
+
+
 
 
 
