12398887f4c421d1133c276a6c50be229492f918
Michael Tai <michael@downloadthebear.com>
2019-09-10 11:48:54 -0400

    - able to present user config update UI - able to process user config update

---
+++ demogodApp/ConfigServerViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ConfigServerViewController.swift
+//  demogodApp
+//
+//
+
+import Foundation
+import AppKit
+
+class ConfigServerViewController: NSViewController {
+    
+    //MARK:- Public API
+    //MARK:
+    var config = RequestServerContext()
+    var updateConfigAction: ((RequestServerContext)->())? = nil
+    
+    //MARK:- User Actions
+    //MARK:
+    @IBAction func requestUpdateConfig(_ sender: NSButton) {
+        defer { dismiss(self) }
+        
+        guard let action = updateConfigAction else { return }
+        action(config)
+    }
+    
+}
--- demogodApp/MainWindowController.swift
+++ demogodApp/MainWindowController.swift
@@ -18,27 +18,12 @@ class MainWindowController: NSWindowController {
         return vc
     }()
     
+    let workQueue = DispatchQueue(label: "com.downloadthebear.MainWindowsC.workq")
+    
     var serverConfig = RequestServerContext()
     
     @IBOutlet weak var portNumber: NSTextField?
     @IBOutlet weak var toolbarCommand: NSTextField?
-    
-    @IBAction func requestToStart(_ sender: NSButton) {
-        if sender.state == .off {
-            startSequence()
-        } else {
-            theViewController.requestToStop()
-        }
-        sender.title = (sender.state == .on ?"" :"")
-    }
-    
-    @IBAction func requestToSendCmd(_ sender: NSButton) {
-        guard let valueToSend = toolbarCommand?.stringValue else {
-            return
-        }
-        theViewController.requestToSend(Value: "\(valueToSend)\n")
-    }
-    
     
     //MARK:- Private functions
     //MARK:
@@ -63,12 +48,51 @@ class MainWindowController: NSWindowController {
             if let port = strongSelf.portNumber?.integerValue, port > 1024 {
                 startCmd = CliCmd.server(.run(CLIRequestServerContext(port: port)))
             }
+            
             var cfg = strongSelf.serverConfig
             cfg.storeUrl = url
+            strongSelf.serverConfig = cfg
+            
             let serveCmd = CliCmd.server(.update(cfg))
             strongSelf.theViewController.requestToStart(withCommands: [startCmd.cmd,
                                                                        serveCmd.cmd])
         }
+    }
+
+    //MARK:- User Actions
+    //MARK:
+    @IBAction func requestToStart(_ sender: NSButton) {
+        if sender.state == .off {
+            startSequence()
+        } else {
+            theViewController.requestToStop()
+        }
+        sender.title = (sender.state == .on ?"" :"")
+    }
+    
+    @IBAction func requestToSendCmd(_ sender: NSButton) {
+        guard let valueToSend = toolbarCommand?.stringValue else {
+            return
+        }
+        theViewController.requestToSend(Value: valueToSend)
+    }
+    
+    @IBAction func requestForUserConfig(_ sender: NSButton) {
+        guard let configUpdateVC = storyboard?
+            .instantiateController(withIdentifier: .init(stringLiteral: "ConfigServerViewController"))
+            as? ConfigServerViewController else {
+            return
+        }
+        configUpdateVC.config = serverConfig
+        configUpdateVC.updateConfigAction = { [weak self] (newConfig) in
+            self?.workQueue.async { [weak self] in
+                guard let strongSelf = self else { return }
+                let serveCmd = CliCmd.server(.update(newConfig))
+                strongSelf.serverConfig = newConfig
+                strongSelf.theViewController.requestToSend(Value: serveCmd.cmd)
+            }
+        }
+        theViewController.presentAsSheet(configUpdateVC)
     }
     
 }
